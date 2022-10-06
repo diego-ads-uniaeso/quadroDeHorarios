becb8063e6c7124a8f7f9afd17ecdc337205d13b
Diego Pereira <diegopereira@MacBook-Air-de-Diego.local>
2022-10-06 19:56:46 -0300

    Criação do Projeto - Quadro de Horários

---
+++ src/main/java/br/edu/uniaeso/quadrodehorarios/QuadroDeHorariosApplication.java
@@ -0,0 +1,13 @@
+package br.edu.uniaeso.quadrodehorarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuadroDeHorariosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuadroDeHorariosApplication.class, args);
+    }
+
+}
