508965999c17b1671de1c709cb65aa60b5adf897
diego-ads-uniaeso <diego.pereira@aluno.uniaeso.edu.br>
2022-10-11 19:24:55 -0300

    Alteração da classe controller Disciplina para DisciplinaController

--- src/main/java/br/edu/uniaeso/quadrodehorarios/controllers/DisciplinaController.java
+++ src/main/java/br/edu/uniaeso/quadrodehorarios/controllers/DisciplinaController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/quadrodehorarios/disciplina")
-public class CDisciplina {
+public class DisciplinaController {
 
     @Autowired
     private DisciplinaService service;
