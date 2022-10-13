4ad4dea87e529c443445ee69eafa833d2f46e90d
Guilherme Augusto Cavalcanti Costa <guilhermecc2@hotmail.com>
2022-10-13 17:16:41 -0300

    Criando classe ProfessorRepositoryImplements e interface ProfessorRepository

---
+++ src/main/java/br/edu/uniaeso/quadrodehorarios/repositories/ProfessorRepository.java
@@ -0,0 +1,21 @@
+package br.edu.uniaeso.quadrodehorarios.repositories;
+
+import br.edu.uniaeso.quadrodehorarios.entities.Professor;
+
+import java.util.List;
+
+public interface ProfessorRepository {
+    Professor save(Professor professor);
+
+    void update(Professor professor);
+
+    void delete(Long id);
+
+    Professor findById(Long id);
+
+    List<Professor> findAll();
+
+    public Professor findByMatricula(String matricula);
+
+    public Professor findByNome(String nome);
+}
---
+++ src/main/java/br/edu/uniaeso/quadrodehorarios/repositories/ProfessorRepositoryImplements.java
@@ -0,0 +1,19 @@
+package br.edu.uniaeso.quadrodehorarios.repositories;
+
+import br.edu.uniaeso.quadrodehorarios.entities.Disciplina;
+import br.edu.uniaeso.quadrodehorarios.entities.Professor;
+
+public class ProfessorRepositoryImplements extends AbstractRepository<Professor,Long> implements ProfessorRepository{
+
+    @Override
+    public Professor findByMatricula(String matricula) {
+        return getEntityManager().createQuery("select d from Professor d where d.matricula = '" + matricula
+                + "'", Professor.class).getSingleResult();
+    }
+
+    @Override
+    public Professor findByNome(String nome) {
+        return getEntityManager().createQuery("select d from Professor d where d.nome = '" + nome
+                + "'", Professor.class).getSingleResult();
+    }
+}
