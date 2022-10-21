48d8fef0dd2a0ebea250b2d867f648fe70fe402f
diego-ads-uniaeso <diego.pereira@aluno.uniaeso.edu.br>
2022-10-20 21:56:25 -0300

    Inclusão de Try Catch na classe de impelemtação do repositório do Professor

--- src/main/java/br/edu/uniaeso/quadrodehorarios/repositories/ProfessorRepositoryImplements.java
+++ src/main/java/br/edu/uniaeso/quadrodehorarios/repositories/ProfessorRepositoryImplements.java
@@ -3,13 +3,19 @@ package br.edu.uniaeso.quadrodehorarios.repositories;
 import br.edu.uniaeso.quadrodehorarios.models.Professor;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NoResultException;
+
 @Repository
 public class ProfessorRepositoryImplements extends AbstractRepository<Professor,Long> implements ProfessorRepository{
 
     @Override
     public Professor findByMatricula(String matricula) {
-        return getEntityManager().createQuery("select d from Professor d where d.matricula = '" + matricula
-                + "'", Professor.class).getSingleResult();
+        try{
+            return getEntityManager().createQuery("select d from Professor d where d.matricula = '" + matricula
+                    + "'", Professor.class).getSingleResult();
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 
     @Override
